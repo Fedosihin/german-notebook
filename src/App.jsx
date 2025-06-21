67df05329425cd670f8d2a6cf1ab48732ad4b734
German <fedosihingerman@yandex.ru>
2025-06-22 02:25:34 +0300

    a lot of stuff added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,116 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [listOfLists, setListOfLists] = useState([
+    { id: 0, title: "first list", text: "first text" },
+    { id: 1, title: "second list", text: "second text" },
+  ]);
+  const [isEditorOpen, setIsEditorOpen] = useState(false);
+  const [activeListIndex, setActiveListIndex] = useState(null);
+
+  const OpenEditorWindow = () => {
+    console.log("Открываю окно редактора");
+    setIsEditorOpen(true);
+  };
+  useEffect(() => {
+    console.dir(listOfLists);
+  }, [listOfLists]);
+  const CloseEditorWindow = () => {
+    console.log("Закрываю окно редактора");
+    setIsEditorOpen(false);
+  };
+
+  const handleListClick = (item, index) => {
+    console.log("Был нажат элемент списка:");
+    console.dir(item);
+    console.log("С индексом:");
+    console.log(index);
+    setActiveListIndex(index);
+    OpenEditorWindow();
+  };
+
+  const CreateEmptyNote = () => {
+    console.log("Добавил пустой элемент в список");
+    setListOfLists([
+      ...listOfLists,
+      {
+        id: listOfLists.length,
+        title: "Пустой заголовок",
+        text: "Пустой текст",
+      },
+    ]);
+  };
+
+  const handleTitleChange = (e) => {
+    const newText = e.target.value;
+    setListOfLists(
+      listOfLists.map((list) =>
+        list.id === activeListIndex ? { ...list, title: newText } : list
+      )
+    );
+  };
+
+  const handleTextChange = (e) => {
+    const newText = e.target.value;
+    setListOfLists(
+      listOfLists.map((list) =>
+        list.id === activeListIndex ? { ...list, text: newText } : list
+      )
+    );
+  };
+
+  const InitRedactorWindow = () => {
+    console.log("Иниц. окно редактора");
+    return (
+      <>
+        {/* <h3>Заголовок:</h3>*/}
+        <h3>{listOfLists[activeListIndex].title}</h3>
+        <input type="text" placeholder="Введите заголовок..." onChange={(e) => {handleTitleChange(e)}} value={listOfLists[activeListIndex].title} />
+        {/* <p>Содержимое модального окна</p> */}
+        <textarea
+          value={listOfLists[activeListIndex].text}
+          onChange={(e) => handleTextChange(e)}
+          placeholder="Введите текст..."
+          rows={10}
+          style={{ width: "100%" }}
+        />
+        {/* <p>{listOfLists[activeListIndex].text}</p> */}
+      </>
+    );
+  };
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {isEditorOpen && (
+          <div className="modal">
+            <div className="modal-content">
+              <h2>Модальное окно</h2>
+              {InitRedactorWindow()}
+              <button onClick={() => CloseEditorWindow()}>Закрыть</button>
+            </div>
+          </div>
+        )}
+        <ul>
+          {listOfLists.map((item, index) => {
+            return (
+              <li
+                key={index}
+                onClick={() => {
+                  handleListClick(item, index);
+                }}
+                style={{ cursor: "pointer" }}
+              >
+                {item.title}
+              </li>
+            );
+          })}
+        </ul>
+        <button onClick={() => CreateEmptyNote()}>Добавить запись</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
