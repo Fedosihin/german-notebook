fbf5532b81cf2e74bd581a6c1cf92a2b09b2a03e
German <fedosihingerman@yandex.ru>
2025-06-30 15:01:54 +0300

    CRITICAL: idk wtf

--- src/components/EmojiButton/EmojiButton.jsx
+++ src/components/EmojiButton/EmojiButton.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import EmojiPicker from "emoji-picker-react";
+// import EmojiPicker from "emoji-picker-react";
 
 const EmojiButton = ({ onEmojiSelect, note }) => {
   const [isPickerOpen, setIsPickerOpen] = useState(false);
