68a50dd67322f91946be2a6993bdadd29d48aba4
German <fedosihingerman@yandex.ru>
2025-06-30 14:12:47 +0300

    fix: emoji button fixed
    - UseState removed. Props used instead.

--- src/components/EmojiButton/EmojiButton.jsx
+++ src/components/EmojiButton/EmojiButton.jsx
@@ -1,25 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import EmojiPicker from "emoji-picker-react";
 
 const EmojiButton = ({ onEmojiSelect, note }) => {
   const [isPickerOpen, setIsPickerOpen] = useState(false);
-  const [selectedEmoji, setSelectedEmoji] = useState(note.emoji || "x");
 
   const handleEmojiClick = (emojiData) => {
-    setSelectedEmoji(emojiData.emoji);
     setIsPickerOpen(false);
     onEmojiSelect(note.id, emojiData.emoji);
   };
-
-//   useE   ffect(() => {
-//     setSelectedEmoji(note.emoji);
-//   }, [selectedEmoji]);
 
   return (
     <div>
       {console.log("emoji render")}
       <button onClick={() => setIsPickerOpen(!isPickerOpen)}>
-        {selectedEmoji || " "}
+        {note.emoji || "Y"}
       </button>
 
       {isPickerOpen && (
