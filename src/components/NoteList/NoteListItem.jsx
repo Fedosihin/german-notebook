2ec8412ba906947dd2f8dd2ce83364df87bd80db
German <fedosihingerman@yandex.ru>
2025-07-18 02:48:04 +0300

    - hotfix i fink

--- src/components/NoteList/NoteListItem.jsx
+++ src/components/NoteList/NoteListItem.jsx
@@ -47,6 +47,22 @@ export default function NoteListItem({
   hideArchive = false,
   hideNotArchive = false,
 }) {
+  if (!('isArchived' in note)) {
+    return(
+      <StyledLi>
+        <StyledEmojiButton
+          onEmojiSelect={onEmojiSelect}
+          note={note}
+        ></StyledEmojiButton>
+        <StyledDiv onClick={onClick} style={style}>
+          <PlaceholderSpan placeholder={"Введите название..."}>
+            {note.title}
+          </PlaceholderSpan>
+        </StyledDiv>
+      </StyledLi>
+    );
+  }
+
   if (hideArchive === true && note.isArchived === true) {
     return;
   } else if (hideNotArchive === true && note.isArchived === false) {
