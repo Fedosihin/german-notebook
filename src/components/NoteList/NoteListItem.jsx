f3f395ad70d769cb68bf7f4ab668700dd413e666
German <fedosihingerman@yandex.ru>
2025-07-18 02:58:45 +0300

    hotfix 55

--- src/components/NoteList/NoteListItem.jsx
+++ src/components/NoteList/NoteListItem.jsx
@@ -47,20 +47,8 @@ export default function NoteListItem({
   hideArchive = false,
   hideNotArchive = false,
 }) {
-  if (!('isArchived' in note) && !hideNotArchive) {
-    return(
-      <StyledLi>
-        <StyledEmojiButton
-          onEmojiSelect={onEmojiSelect}
-          note={note}
-        ></StyledEmojiButton>
-        <StyledDiv onClick={onClick} style={style}>
-          <PlaceholderSpan placeholder={"Введите название..."}>
-            {note.title}
-          </PlaceholderSpan>
-        </StyledDiv>
-      </StyledLi>
-    );
+   if (!('isArchived' in note)) {
+    note.isArchived = false;
   }
 
   if (hideArchive === true && note.isArchived === true) {
